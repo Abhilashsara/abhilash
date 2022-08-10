521c322b85d04e43608ec8bdaca06f41fc9c96cd
Abhilashsara <saraswatabhilash997@gmail.com>
2022-08-10 17:15:57 +0530

    commit

--- Day7_Assignment3/myscript.js
+++ Day7_Assignment3/myscript.js
@@ -1,6 +1,18 @@
+// var info ={
+//     name:" Abhilash Saraswat ",
+//     age:21,
+//     Address:{City:"Mathura",State:"U.P",pincode:201111},
+//     Hobbies:["Listening Music","Cooking","Travelling"],
+//     details:
+//     function()
+//     {
+//         return this.name +"from" +this.Address.City;
+//     },
+// };
+// document.write(info.details());
+// document.body.style.backgroundColor="yellowgreen";
+// document.body.style.color="purple";;
+// document.body.style.fontSize="100px";
+// document.body.style.padding="70px 100px";
 
-<script>
-	function func1(){}
-	func1.displayName="someName"
-	console.log(func1.displayName)
-</script>
+
